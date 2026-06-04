c23a8b227092c87e83592fd685c6abb850700479
nakakasu134 <tomokung745@gmail.com>
2026-06-04 15:40:37 +0900

    Refactor About Me section with fade-in functionality and update styles for improved user experience

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ scrollBtn.addEventListener("click", function () {
         behavior: "smooth"
     });
 });
+
+const fadeContainers = document.querySelectorAll(".fade-container");
+
+fadeContainers.forEach(function (container) {
+    const toggle = container.querySelector(".toggle");
+    const fadeBox = container.querySelector(".fade-box");
+    const hiddenText = "を隠す";
+
+    toggle.addEventListener("click", function () {
+        fadeBox.classList.toggle("is-visible");
+
+        if (fadeBox.classList.contains("is-visible")) {
+            toggle.textContent += hiddenText;
+        } else {
+            toggle.textContent = toggle.textContent.replace(hiddenText, "");
+        }
+    });
+});
+
+const vidIframes = document.querySelectorAll(".vid-iframe");
+vidIframes.forEach(function (iframe) {
+    const toggle = iframe.parentElement.querySelector(".toggle");
+    const hiddenText = "を隠す";
+
+    toggle.addEventListener("click", function () {
+        iframe.classList.toggle("is-visible");
+
+        if (iframe.classList.contains("is-visible")) {
+            toggle.textContent += hiddenText;
+        } else {
+            toggle.textContent = toggle.textContent.replace(hiddenText, "");
+        }
+    });
+});
