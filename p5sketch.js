1823d307fbd358bdeda5a6ff3f3491eefd334481
nakakasu134 <tomokung745@gmail.com>
2026-06-04 17:42:00 +0900

    Add p5.js integration for dynamic canvas

---
+++ p5sketch.js
@@ -0,0 +1,9 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    background(0);
+}
